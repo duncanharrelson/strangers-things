61a27df75350d6246c990ec4f3182fa5605dd1e4
“WynnVN” <wynnvannorman20@gmail.com>
2023-09-24 18:19:51 -0500

    first push. LoginForm.jsx, LogoutForm.jsx, App.jsx

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+// import CreateNewUser from './api/index.js
+import {Routes, Route} from "react-router-dom"
+
+
+function LogIn ({setToken}) {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError]= useState(null);
+    const [valid, setValid]=useState(false);
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        try {
+            const response= await CreateNewUser()
+            console.log(result);
+            setToken(result.token);
+        } catch (error) {
+            setError(error.message);
+        }
+        
+    }
+        return (
+            <>
+            <h2>Log In!!</h2>
+            {error && <p>{error}</p>}
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username: <input value={username} onChange={(e)=> setUsername(e.target.value)} />
+                {username.length <=8 ? (
+                <p>Username must be 8 characters</p>) : (<p>Username is good to go</p>
+                )}
+            </label>
+            <label>
+                Password: <input type='password' value={password} onChange={(e)=> setPassword(e.target.value)}/>
+    
+            </label>
+            <button>Submit</button>
+        </form>
+        </>
+    )};
+    // 2306-FTB-ET-WEB-AM
+
+
+export default LogIn
