4da5edda268a0a39245cd96c062ae3c005057e6b
Victor Abadie <victor_abadie@aol.com>
2023-09-26 09:38:37 -0500

    RegisterForm functionality added

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,42 +1,43 @@
 import { useState } from "react";
+import  registerUser  from "../api"
 
-const COHORT = "2306-ftb-et-web-am"
-const API_URL = `https://strangers-things.herokuapp.com/${COHORT}`
+// const COHORT = "2306-ftb-et-web-am"
+// const API_URL = `https://strangers-things.herokuapp.com/${COHORT}`
 
 
-const RegisterForm = ({ token }) => {
-    const [successMessage, setSuccessMessage] = useState(null);
+const SignUpForm = () => {
     const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
     const [error, setError] = useState(null);
 
-    async function handleClick(e) {   
-        e.preventDefault();     
+    async function handleSubmit(event) {
+        event.preventDefault();
         try {
-            const response = await fetch(API_URL, {
-                method: "GET",               
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`,
-                },
-            })
-            const result = await response.json();
-            console.log(response)
-            setSuccessMessage(result.message);   
-            setUsername(result.data.username);
+            const response= await registerUser()
+            console.log(result);
+            setToken(response);
         } catch (error) {
             setError(error.message);
         }
     }
 
     return (
-        <>
-            <h2 className="auth">Authenticate!</h2>
-            {username && <p>Welcome, {username}!</p>}
-            {successMessage && <p>{successMessage}</p>}
-            {error && <p>{error}</p>}
-            <button onClick={handleClick}>Authenticate Token!</button>
+        <>        
+            <h2 className="sign-up">Sign Up</h2>
+            { error && <p>{error}</p> }
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Username: <input value={username} onChange={(e) => setUsername(e.target.value)} minLength={8} required/>
+                </label>
+                <br />
+                <label>
+                    Password: <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} minLength={8} required/>
+                </label>
+                <br />
+                <button type="submit">Submit</button>
+            </form>        
         </>
     )
 }
 
-export default RegisterForm
+export default SignUpForm;
