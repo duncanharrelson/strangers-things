72191f1df79bf3f44842f7897c7487cda6023a9d
Victor Abadie <victor_abadie@aol.com>
2023-09-26 23:42:20 -0500

    Minor CSS + Fixed Search Functionality on HomePage

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const NavBar = () => {
     return (
-        <div id="navbar">
+        <div className="navbar">
             <Link to="/">Home</Link>
             <Link to="/profile">Profile</Link>
             <Link to="/login">Login</Link>
